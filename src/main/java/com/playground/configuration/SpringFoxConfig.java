69f2303bbefb227a481e98ef4a7ae98100914b86
Mikhail Shumenko <mykhailo.shumenko@auto1.com>
2020-02-28 00:42:58 +0200

    SPIKE add swagger

---
+++ src/main/java/com/playground/configuration/SpringFoxConfig.java
@@ -0,0 +1,30 @@
+package com.playground.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Swagger docs
+ * <p>
+ * read more at https://www.baeldung.com/swagger-2-documentation-for-spring-rest-api
+ * <p>
+ * Documentation is available at http://localhost:8080/playground/swagger-ui.html
+ */
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
