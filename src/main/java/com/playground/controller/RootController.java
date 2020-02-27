fc1165dbf7c11304f6aaebead701a3930ac9e071
Mikhail Shumenko <mykhailo.shumenko@auto1.com>
2020-02-28 00:45:08 +0200

    SPIKE add root controller

---
+++ src/main/java/com/playground/controller/RootController.java
@@ -0,0 +1,13 @@
+package com.playground.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @RequestMapping("/")
+    public String root() {
+        return "forward:/playground/swagger-ui.html";
+    }
+}
