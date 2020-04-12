c0813122b9b95e5377aca0453289cfb52fb9d0bd
Mikhail Shumenko <mykhailo.shumenko@auto1.com>
2020-04-12 03:53:41 +0300

    SPIKE disable consul in tests

--- src/test/java/com/playground/PlaygroundServiceApplicationTests.java
+++ src/test/java/com/playground/PlaygroundServiceApplicationTests.java
@@ -3,7 +3,11 @@ package com.playground;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+        properties = {
+                "spring.cloud.consul.enabled=false"
+        }
+)
 class PlaygroundServiceApplicationTests {
 
     @Test
