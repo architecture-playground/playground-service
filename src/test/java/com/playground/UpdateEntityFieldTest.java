4e1c59b407c2d9c6c8012ff05e3cac3b5c31f4ea
Mikhail Shumenko <mykhailo.shumenko@auto1.com>
2020-04-14 14:06:20 +0300

    SPIKE add entity fields test

---
+++ src/test/java/com/playground/UpdateEntityFieldTest.java
@@ -0,0 +1,34 @@
+package com.playground;
+
+import com.playground.model.PlaygroundEntity;
+import com.playground.repository.PlaygroundEntityRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@PlaygroundIntegrationTest
+public class UpdateEntityFieldTest {
+
+    @Autowired
+    private PlaygroundEntityRepository repository;
+
+    @Test
+    void setFieldToNull() {
+        PlaygroundEntity toSave = new PlaygroundEntity();
+        toSave.setObjectId(UUID.randomUUID());
+        toSave.setComment("This is comment!");
+
+        UUID savedId = repository.save(toSave).getId();
+        repository.findById(savedId).ifPresent(
+                it -> {
+                    it.setComment(null);
+                    repository.save(it);
+                });
+
+        assertThat(repository.findById(savedId))
+                .hasValueSatisfying(it -> assertThat(it.getComment()).isNull());
+    }
+}
