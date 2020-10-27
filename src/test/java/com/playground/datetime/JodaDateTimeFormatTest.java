dedaf52f256583418cb3fca434555690fb66b57d
Mikhail Shumenko <mykhailo.shumenko@auto1.com>
2020-10-27 17:02:25 +0200

    SPIKE : add more serialization tests

--- src/test/java/com/playground/datetime/JodaDateTimeFormatTest.java
+++ src/test/java/com/playground/datetime/JodaDateTimeFormatTest.java
@@ -1,6 +1,7 @@
 package com.playground.datetime;
 
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,6 +11,14 @@ public class JodaDateTimeFormatTest {
     @Test
     void testParseFromString() {
         DateTime time = DateTime.parse("2020-12-25T01:00:00");
+
+        assertThat(time).isNotNull();
+    }
+
+    @Test
+    void testParseWithTimeZone() {
+        DateTime time = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSSSSSZ")
+                .parseDateTime("2020-10-10 10:10:00.000000+00");
 
         assertThat(time).isNotNull();
     }
