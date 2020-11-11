d0c4e1bd5659069a9445e925c5944a76d01913f5
Mikhail Shumenko <mykhailo.shumenko@auto1.com>
2020-11-11 16:04:28 +0200

    SPIKE : add more logging tests

--- src/test/java/com/playground/logging/MessageFormatTest.java
+++ src/test/java/com/playground/logging/MessageFormatTest.java
@@ -4,14 +4,16 @@ import org.junit.jupiter.api.Test;
 
 import java.text.MessageFormat;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MessageFormatTest {
 
     @Test
-    void testCollectionInMessageFormat() {
+    void testMapInMessageFormat() {
         Map<String, String> map = new HashMap<>();
 
         map.put("ke1", "value1");
@@ -19,5 +21,12 @@ public class MessageFormatTest {
         map.put("ke3", "value3");
 
         assertThat(MessageFormat.format("{0}", map)).isEqualTo("{ke1=value1, ke3=value3, ke2=value2}");
+    }
+
+    @Test
+    void testStreamInMessageFormat() {
+        Stream<String> stream = List.of("one", "two", "three").stream();
+
+        assertThat(MessageFormat.format("{0}", stream)).startsWith("java.util.stream.ReferencePipeline$");
     }
 }
