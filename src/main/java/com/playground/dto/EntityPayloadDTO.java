fce9b57553d1d062944b0e9fa520b3c0674c1ef7
Mykhailo Shumenko <mykhailo.shumenko@auto1.com>
2023-09-05 02:31:42 +0200

    add "id" to EntityPayloadDTO, fix testcontainers for Mac M1 (update libs)

--- src/main/java/com/playground/dto/EntityPayloadDTO.java
+++ src/main/java/com/playground/dto/EntityPayloadDTO.java
@@ -3,9 +3,12 @@ package com.playground.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @NoArgsConstructor
 @Data
 public class EntityPayloadDTO {
 
+    private UUID id;
     private Payload payload;
 }
--- src/test/java/com/playground/testconfig/TestContainerDatabaseConfiguration.java
+++ src/test/java/com/playground/testconfig/TestContainerDatabaseConfiguration.java
@@ -5,22 +5,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.DockerImageName;
 
 import javax.sql.DataSource;
 import java.time.Duration;
 
+import static org.testcontainers.containers.PostgreSQLContainer.DEFAULT_TAG;
+
 @Configuration
-public class DatabaseConfiguration {
+public class TestContainerDatabaseConfiguration {
 
-    @Bean
-    public PostgreSQLContainer<?> postgreSQLContainer() {
-        PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:9.6.2")
-                .withUsername("postgres")
-                .withPassword("system")
-                .withStartupTimeout(Duration.ofSeconds(30));
+    public static final String USERNAME = "postgres";
+    public static final String PASSWORD = "system";
 
-        container.start();
-        return container;
+    private static final PostgreSQLContainer<?> CONTAINER = new PostgreSQLContainer<>(DockerImageName.parse("postgres").withTag(DEFAULT_TAG))
+            .withUsername(USERNAME)
+            .withPassword(PASSWORD)
+            .withEnv("POSTGRESQL_USER", USERNAME)
+            .withEnv("POSTGRESQL_PASSWORD", PASSWORD)
+            .withExposedPorts(PostgreSQLContainer.POSTGRESQL_PORT);
+
+    static {
+        CONTAINER.setWaitStrategy(Wait.forListeningPort()
+                .withStartupTimeout(Duration.ofSeconds(30)));
+
+        CONTAINER.start();
     }
 
     @Bean
